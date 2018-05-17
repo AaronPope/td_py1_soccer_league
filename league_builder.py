20963cb476304f70dd37413c7fe9858805149ac5
aaronpope <aaronpope.dev@gmail.com>
2018-05-17 07:37:14 -0400

    Fixed a typo in if __name__ check Added 'pass' placeholder to make red marks go away (until I get started)

---
+++ league_builder.py
@@ -0,0 +1,38 @@
+# Aaron Pope
+# 05/17/2018
+# Treehouse TechDegree - Python, Unit 1: Build a Soccer League
+
+
+# Don't execute unless this is the main script
+if __name__ == "__main__":
+    pass # just until I get rolling on this thing!
+
+# Divide players into three teams
+    # Sharks
+    # Dragons
+    # Raptors
+# Teams should have the same number of players
+    # and the experienced players should be divided equally across the three teams
+# Create a text file names 'teams.txt' that includes:
+    # the name of a team
+    # the players on the team
+# Sample output
+    # Sharks
+    # Frank Jones, YES, Jim and Jan Jones
+    # Sarah Palmer, YES, Robin and Sari Washington
+    # Joe Smith, NO, Bob and Jamie Smith
+
+# EXTRA CREDIT (1/2)
+    # Create 18 text files, to serve as welcome letters to the players' guardians
+        # 1 file should be created for each player
+        # Use the players name as the name of the file
+            # lowercase
+            # underscore between first and last name
+            # E.g. - "jimmy_johns.txt
+
+# EXTRA CREDIT (2/2)
+    # Ensure that each file includes the following: 
+        # Begin with "Dear [guardian(s) name(s)]"
+        # player's name
+        # team name
+        # date & time of first practice
