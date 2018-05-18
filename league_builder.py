1f76ebaa1479d0b7093767c5645cb577a78073c9
aaronpope <aaronpope.dev@gmail.com>
2018-05-18 19:06:54 -0400

    Have a basic function that sequentially places players on teams, but it doesn't yet account for experience

--- league_builder.py
+++ league_builder.py
@@ -3,14 +3,45 @@
 # Treehouse TechDegree - Python, Unit 1: Build a Soccer League
 
 
+import csv
+
 # Don't execute unless this is the main script
 if __name__ == "__main__":
-    pass # just until I get rolling on this thing!
 
 # Divide players into three teams
     # Sharks
     # Dragons
-    # Raptors
+    # Raptors    
+    sharks = ("Sharks", [])
+    dragons = ("Dragons", [])
+    raptors = ("Raptors", [])
+
+    def add_player_to_team (player, team):
+        team_name, player_list = team
+        player_list.append(player)
+        return (team_name, player_list)
+
+    with open ("soccer_players.csv") as file:
+        reader = csv.DictReader(file)
+        soccer_players = list(reader)
+
+        for soccer_player in soccer_players:
+            player_info = ( soccer_player["Name"], 
+                            soccer_player["Soccer Experience"], 
+                            soccer_player["Guardian Name(s)"])
+            if len(sharks[1]) < 6:
+                add_player_to_team(player_info, sharks)
+            elif len(dragons[1]) < 6:
+                add_player_to_team(player_info, dragons)
+            else:
+                add_player_to_team(player_info, raptors)
+
+    print (sharks)
+    print (dragons)
+    print (raptors)
+
+
+
 # Teams should have the same number of players
     # and the experienced players should be divided equally across the three teams
 # Create a text file names 'teams.txt' that includes:
