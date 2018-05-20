866e460db9fa7e05b0bf00f048bdb743b788d864
aaronpope <aaronpope.dev@gmail.com>
2018-05-20 09:19:57 -0400

    Decided against prepending teams.
    Instead, created a welcome_letters folder within output

--- utilities.py
+++ utilities.py
@@ -4,7 +4,7 @@ def generate_welcome_letter(player, team):
     name = player["name"].split()
     first_name = name[0]
     last_name = name[1]
-    with open ("output/{}_{}.txt".format(first_name, last_name), "w") as file:
+    with open ("output/welcome_letters/{}_{}.txt".format(first_name, last_name), "w") as file:
         file.write ("""Dear {},\n
 {} will be playing on the {}.
 Their first practice is on {}.""".format(
