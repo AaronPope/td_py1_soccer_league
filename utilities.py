3d09fa024339d91964cc78563347a4338bc07186
aaronpope <aaronpope.dev@gmail.com>
2018-05-20 11:16:11 -0400

    Fixed file name formatting for welcome letters (title case -> lower case)

--- utilities.py
+++ utilities.py
@@ -4,7 +4,8 @@ def generate_welcome_letter(player, team):
     name = player["name"].split()
     first_name = name[0]
     last_name = name[1]
-    with open ("output/welcome_letters/{}_{}.txt".format(first_name, last_name), "w") as file:
+    with open ("output/welcome_letters/{}_{}.txt".format(
+        first_name.lower(), last_name.lower()), "w") as file:
         file.write ("""Dear {},\n
 {} will be playing on the {}.
 Their first practice is on {} at {}.""".format(
